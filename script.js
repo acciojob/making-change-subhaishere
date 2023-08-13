6d172350caa98a74645410de690bd0d6487aae2c
subhaishere <60627114+subhaishere@users.noreply.github.com>
2023-08-14 01:29:26 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,10 @@
 const makeChange = (c) => {
-  // your name here
+  return {
+    q: Math.floor(c / 25),
+    d: Math.floor((c % 25) / 10),
+    n: Math.floor(((c % 25) % 10) / 5),
+    p: c % 5,
+  };
 };
 
 // Do not the change the code below
